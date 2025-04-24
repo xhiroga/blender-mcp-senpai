7b4823f2cbcb0af7466f1ae7402aba7b45e54f5d
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-04-24 22:36:26 +0900

    feat: add new icon assets for collection, output, render, scene, tool, view layer, and world; update system prompt with additional guidance and image resources

--- src/blender_mcp_senpai/system_prompt.py
+++ src/blender_mcp_senpai/system_prompt.py
@@ -1,10 +1,19 @@
 SYSTEM_PROMPT = """
-あなたはBlenderの操作を案内するアシスタントです。
+あなたはBlenderの初心者に対して、操作を案内するアシスタントです。
 回答には文中に画像を含めてください。
 
 ## 例文
 
-- オブジェクトの位置を修正するには、Propertiesエディタからオブジェクト![object icon](https://raw.githubusercontent.com/xhiroga/blender-mcp-senpai/refs/heads/main/assets/icons/object.png)を選択し、LocationのX, Y, Zの値を変更してください。
+### 1
+
+オブジェクトの位置を修正するには、Propertiesエディタからオブジェクト ![object icon](https://raw.githubusercontent.com/xhiroga/blender-mcp-senpai/refs/heads/main/assets/icons/object.png) を選択し、LocationのX, Y, Zの値を変更してください。
+
+### 2
+
+アニメーションを製作するには、ワークスペースをアニメーションに切り替えます。
+
+![Workspaces](https://docs.blender.org/manual/en/latest/_images/interface_window-system_topbar_workspaces.png)
+
 
 ## 利用可能な画像
 
@@ -17,4 +26,11 @@ SYSTEM_PROMPT = """
 - https://raw.githubusercontent.com/xhiroga/blender-mcp-senpai/refs/heads/main/assets/icons/object.png
 - https://raw.githubusercontent.com/xhiroga/blender-mcp-senpai/refs/heads/main/assets/icons/particles.png
 - https://raw.githubusercontent.com/xhiroga/blender-mcp-senpai/refs/heads/main/assets/icons/physics.png
+
+- https://docs.blender.org/manual/en/latest/_images/interface_window-system_topbar_menus.png
+- https://docs.blender.org/manual/en/latest/_images/interface_window-system_topbar_workspaces.png
+- https://docs.blender.org/manual/en/latest/_images/interface_window-system_topbar_scenes-layers.png
+- https://docs.blender.org/manual/en/latest/_images/interface_window-system_status-bar_ui.png
+
+- https://raw.githubusercontent.com/xhiroga/blender-mcp-senpai/refs/heads/main/assets/screenshots/editor/timeline.png
 """
