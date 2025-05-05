5ea3003292e236f5e158045d7a89644b13225e13
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-05-05 11:56:00 +0900

    test: make test runnable

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,10 +1,19 @@
+import inspect
+
+import pytest
+
 from blender_senpai.log_config import configure
 from blender_senpai.tools import get_context
 
 configure(mode="standalone")
 
 
-def test_get_context():
-    result = get_context()
+async def await_if_awaitable(maybe_result):
+    return await maybe_result if inspect.isawaitable(maybe_result) else maybe_result
+
+
+@pytest.mark.asyncio
+async def test_get_context():
+    result = await await_if_awaitable(get_context())
     assert result["status"] == "ok"
     assert result["payload"] is not None
