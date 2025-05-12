283fa345d815f59a219728294970f097a2f64e79
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-05-12 15:42:00 +0900

    fix: workaround for github actions

--- tests/repositories/test_api_key_repository.py
+++ tests/repositories/test_api_key_repository.py
@@ -1,4 +1,7 @@
+import sys
 from logging import getLogger
+
+import pytest
 
 from blender_senpai.repositories.api_key_repository import ApiKeyRepository
 from blender_senpai.types.api_key import ApiKey
@@ -6,6 +9,9 @@ from blender_senpai.types.api_key import ApiKey
 logger = getLogger(__name__)
 
 
+@pytest.mark.skipif(
+    sys.platform.startswith("linux"), reason="Workaround. GitHub Actions has no GUI."
+)
 def test_crud():
     ApiKeyRepository.save("test", ApiKey("test"))
     assert ApiKeyRepository.get("test") == ApiKey("test")
